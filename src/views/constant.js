b28a4ec12e56be5467db28bfdabfc1fcc7c11128
drsselva <ss@gritdigitech.com>
2023-05-23 19:44:55 +0530

    changes-2305

--- src/views/constant.js
+++ src/views/constant.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://44.202.89.70:8989/api/";
+const BASE_URL = "http://44.203.0.129:8989/api/";
 export const BASE_URL1 = `${BASE_URL}`;
 export const URL_ALLCOURSES = `${BASE_URL}getAllCourse`;
-export const BASE_URLAPI = "http://44.202.89.70:8989/";
+export const BASE_URLAPI = "http://44.203.0.129:8989/";
 
